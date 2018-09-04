c7534fe662a72796114f80d854c7481a4ac6fe45
Aerendir <hello@aerendir.me>
2018-09-04 18:39:10 +0200

    Improve building
    - Don't move `functions.dist.php`;
    - Move `screenshot.png`.

--- gulpfile.js
+++ gulpfile.js
@@ -1,62 +1,72 @@
 var gulp = require('gulp'),
-    concat = require('gulp-concat'),
-    rename = require('gulp-rename'),
-    sass = require('gulp-sass'),
-    merge = require('merge-stream'),
-    del = require('del');
+  concat = require('gulp-concat'),
+  rename = require('gulp-rename'),
+  sass = require('gulp-sass'),
+  merge = require('merge-stream'),
+  del = require('del');
 
 // Move all PHP files to the build/theme folder
 gulp.task('move-php-files', function(done){
-    gulp.src(['src/*.php', '!src/config.dist.php'])
-        .pipe(gulp.dest('build/theme'));
+  gulp.src(['src/*.php', '!src/config.dist.php', '!src/functions.dist.php'])
+    .pipe(gulp.dest('build/theme'));
 
-    gulp.src('src/lib/*.php')
-        .pipe(gulp.dest('build/theme/lib'));
+  gulp.src('src/lib/*.php')
+    .pipe(gulp.dest('build/theme/lib'));
 
-    // Inform Gulp this task is done
-    done();
+  // Inform Gulp this task is done
+  done();
+});
+
+// Move the miniature of the theme to the build/theme folder
+gulp.task('move-screenshot', function(done){
+  gulp.src(['src/screenshot.png'])
+    .pipe(gulp.dest('build/theme'));
+
+  // Inform Gulp this task is done
+  done();
 });
 
 // Move WP Bootstrap Walker
 gulp.task('move-bootstrap-walker', function(done){
-    gulp.src('vendor/wp-bootstrap/wp-bootstrap-navwalker/class-wp-bootstrap-navwalker.php')
-        .pipe(gulp.dest('build/theme/lib'));
+  gulp.src('vendor/wp-bootstrap/wp-bootstrap-navwalker/class-wp-bootstrap-navwalker.php')
+    .pipe(gulp.dest('build/theme/lib'));
 
-    // Inform Gulp this task is done
-    done();
+  // Inform Gulp this task is done
+  done();
 });
 
 gulp.task('clean-build', function (done) {
-    return del(['build/**/*']);
+  return del(['build/**/*']);
 });
 
 // Build CSSes from SASSes
 gulp.task('build-css', function (done) {
-    var cssStream = gulp.src('src/style.css');
-    var sassStream = gulp.src('src/scss/style.scss')
-        .pipe(sass.sync().on('error', sass.logError))
-        .pipe(gulp.dest('build/tmp'));
+  var cssStream = gulp.src('src/style.css');
+  var sassStream = gulp.src('src/scss/style.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('build/tmp'));
 
-    var mergedStream = merge(cssStream, sassStream)
-        .pipe(concat('test.css'))
-        .pipe(rename('style.css'))
-        .pipe(gulp.dest('build/theme'));
+  var mergedStream = merge(cssStream, sassStream)
+    .pipe(concat('test.css'))
+    .pipe(rename('style.css'))
+    .pipe(gulp.dest('build/theme'));
 
-    done();
+  done();
 });
 
 // Include Bootstrap Bundle
 gulp.task('move-bootstrap-bundle', function (done) {
-    gulp.src('node_modules/bootstrap/dist/js/bootstrap.bundle.js')
-        .pipe(gulp.dest('build/theme/js'));
+  gulp.src('node_modules/bootstrap/dist/js/bootstrap.bundle.js')
+    .pipe(gulp.dest('build/theme/js'));
 
-    done();
+  done();
 });
 
 gulp.task('build', gulp.series(
-    'clean-build',
-    'build-css',
-    'move-bootstrap-bundle',
-    'move-php-files',
-    'move-bootstrap-walker'
+  'clean-build',
+  'build-css',
+  'move-bootstrap-bundle',
+  'move-php-files',
+  'move-bootstrap-walker',
+  'move-screenshot'
 ));
